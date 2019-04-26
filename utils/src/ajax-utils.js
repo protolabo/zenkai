df9907dffd0879ffd59fcf1d68b3d53b3de3d3d5
Louis-Edouard <lelafontant@yahoo.ca>
2019-04-26 10:51:00 -0400

    added: POST and DELETE to Ajax

--- utils/src/ajax-utils.js
+++ utils/src/ajax-utils.js
@@ -1,12 +1,30 @@
-/** @module ajax */
+import { isFunction } from "./datatype/index.js";
 
-export const XHR = function (file, callback) {
-    var xhr = new XMLHttpRequest();
+const xhrHandler = function(type, file, callback) {
+    var xhr = new XMLHttpRequest();    
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            callback(xhr.responseText);
+            if (isFunction(callback)) {
+                callback(xhr.responseText);
+            }
         }
     };
-    xhr.open('GET', file, true);
-    xhr.send();
+    xhr.open(type, file, true);
+
+    return xhr;
 };
+
+export function GET (file, callback) {
+    var xhr = xhrHandler('GET', file, callback);
+    xhr.send();
+}
+
+export function POST(file, form, callback) {
+    var xhr = xhrHandler('POST', file, callback);
+    xhr.send(form);
+}
+
+export function DELETE(file, form, callback) {
+    var xhr = xhrHandler('DELETE', file, callback);
+    xhr.send(form);
+}
