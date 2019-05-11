bd0ed13fe6196a22f5db733cc869eef2d2bc1546
Louis-Edouard <lelafontant@yahoo.ca>
2019-05-10 23:41:08 -0400

    packaging changed

--- README.md
+++ README.md
@@ -5,12 +5,15 @@ Javascript libraries
 - DOM: DOM manipulation
 - TYPE: DataType manipulation
 - AJAX: AJAX helpers (GET, POST, DELETE, PUT)
-- MATH: Math functions
-- PATH: Path utils
-- URI: Uri helper functions
+- MATH: Math helpers
+- PATH: Path helpers
+- URI: Uri helpers
 
 ## Components
-- FORM: A collection of form component: floating label, selector, slider...
+- FORM: A collection of form components 
+  - Floating Label
+  - Selector
+  - Slider
 
 # Documentation
 [Learn more about the library](https://protolabo.github.io/jslib/index.html).
---
+++ scripts/build.js
@@ -0,0 +1,37 @@
+/*eslint no-console: ["error", { allow: ["warn", "error"] }] */
+
+var fs = require('fs');
+var path = require('path');
+var CleanCSS = require('clean-css');
+
+const DIRECTORY = './dist/css';
+
+var bundle = new CleanCSS({
+    sourceMap: true,
+    rebaseTo: `${DIRECTORY}/form.min.css`
+}).minify([
+    './src/components/form/form.css'
+]);
+
+// log bundle result
+outputFeedback(bundle.errors, true);
+outputFeedback(bundle.warnings);
+// write bundle file
+if (!fs.existsSync(DIRECTORY)) {
+    fs.mkdirSync(DIRECTORY, { recursive: true });
+}
+output({ path: `${DIRECTORY}/form.min.css` }, bundle.styles);
+output({ path: `${DIRECTORY}/form.min.map` }, bundle.sourceMap);
+
+function outputFeedback(messages, isError) {
+    var prefix = isError ? '\x1B[31mERROR\x1B[39m:' : 'WARNING:';
+
+    messages.forEach(function (message) {
+        console.error('%s %s', prefix, message);
+    });
+}
+
+function output(options, minified) {
+    fs.writeFileSync(options.path, minified, 'utf8');
+}
+
